6baed6d081cac10bda7ece7e0cb8a2a4f5d3b75c
Nguyen Hoang Hiep <hiepnh@runsystem.net>
2020-08-06 22:26:42 +0700

    add

---
+++ my-app/src/main/java/runsystem/com/vn/MyAppApplication.java
@@ -0,0 +1,13 @@
+package runsystem.com.vn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppApplication.class, args);
+	}
+
+}
---
+++ my-app/src/test/java/runsystem/com/vn/MyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package runsystem.com.vn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
